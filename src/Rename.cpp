4480f1d5b54a485bdb03cf0c114a6ef06fdd6afe
tianzhendong <1203886034@qq.com>
2022-06-20 09:09:03 +0800

    修改readme，创建release

---
+++ src/Rename.cpp
@@ -0,0 +1,145 @@
+//
+// Created by 12038 on 2022/6/16.
+//
+#include <QDateTime>
+#include "Rename.h"
+#include <QProcess>
+
+Rename::Rename(QObject *) {
+
+}
+
+Rename::~Rename() {
+
+}
+
+void Rename::renameFileSlot(const QString &filePath, const QString &strReplaced, const QString &strNew) {
+//    filePath.lastIndexOf('/')
+    renameFile(filePath, strReplaced, strNew);
+}
+
+void Rename::renameFile(const QString &filePath, const QString &strReplaced, const QString &strNew) {
+    QFile file(filePath);
+    QFileInfo fileInfo(filePath);
+    qDebug()<<fileInfo.path();
+    qDebug()<<fileInfo.absolutePath();
+    qDebug()<<fileInfo.fileName();
+    qDebug()<<fileInfo.suffix();
+    qDebug()<<fileInfo.baseName();
+    QString baseName = fileInfo.baseName();
+    QString newBaseName = baseName;
+
+    QString oldFileName = file.fileName();
+    qDebug()<< "old:" <<oldFileName;
+    QString newFileName = oldFileName;
+
+    if(renameMethodFlag == "时间戳") {
+//        if(!baseName.contains("flyrecord")) return "";
+        QString timeStampStr = baseName.section('-', -1, -1);
+        QString firstName = baseName.section('-', -2, 0);
+        qint64 timeStamp = timeStampStr.toLongLong();
+        qDebug() << "时间戳" << timeStamp;
+        QDateTime dateTime = QDateTime::fromMSecsSinceEpoch(timeStamp);
+        if((!dateTime.isValid()) || (timeStamp == 0) || (dateTime > QDateTime::currentDateTime())){
+            emit newFileNameSignal(tr("时间戳有误"));
+            return;
+        };
+        QString time = dateTime.toString("yyyyMMddhhmmss"); //时间戳非毫秒级
+        QString time1 = dateTime.toString("yyyy-MM-dd hh:mm:ss");
+        qDebug() << time1;
+
+        newBaseName = firstName + '-' +time;
+    }
+    else if(renameMethodFlag == "替换"){
+        qDebug()<<"mode"<<"替换";
+        if(!baseName.contains(strReplaced)) {
+            emit newFileNameSignal("文件名中不存在输入的字符串");
+            return;
+        };
+        if(strNew == ""){
+            emit newFileNameSignal("新替换的字符串为空");
+            return;
+        }
+        if(strReplaced == "") {
+            newBaseName = strNew;
+        }
+        else{
+            newBaseName = baseName.replace(strReplaced, strNew);
+            qDebug()<<"newBaseName:"<<newBaseName;
+        }
+    }
+    dirFiles.clear();
+    dirFiles = getFiles(fileInfo.absolutePath());
+    newFileName = fileInfo.absolutePath() + '/' + newBaseName + '.' + fileInfo.suffix();
+    if(dirFiles.contains(newFileName)){
+        newFileName = fileInfo.absolutePath() + '/' + newBaseName + '-' + baseName +'.' + fileInfo.suffix();
+    }
+    qDebug()<< "new:" <<newFileName;
+
+    file.rename(newFileName);
+    emit newFileNameSignal(newFileName);
+}
+
+void Rename::getFilesSlot(const QString &filesDirPath, const QString &strReplaced, const QString &strNew) {
+    //获取文件夹下所有的文件
+    QStringList files= this->getFiles(filesDirPath);
+//    emit oldFileNameSignal(files);
+    QStringList newFiles;
+    for (int i = 0; i < files.count(); i++){
+        QString fileName = files.at(i);
+        emit oldFileNameSignal(fileName);
+        renameFile(fileName, strReplaced, strNew);
+//        emit newFileNameSignal(newFile);
+        emit process(i + 1, files.count());
+    }
+}
+
+QStringList Rename::getFiles(const QString &filesDirPath) {
+    QStringList files;
+    QDir filesDir(filesDirPath);
+    QList<QFileInfo> fileInfoList = filesDir.entryInfoList(QDir::Files);
+    for(int i = 0; i < fileInfoList.count(); i++){
+        QFileInfo fileInfo = fileInfoList.at(i);
+        if(fileInfo.isDir()){
+//            QStringList subFiles= getFiles(fileInfo.absoluteFilePath());
+//            files.append(subFiles);
+            continue;
+        }
+        else{
+            QString fileName = fileInfo.absoluteFilePath();
+            files.append(fileName);
+        }
+    }
+    return files;
+}
+
+void Rename::checkBoxChangedSlot(int) {
+
+}
+
+void Rename::comboxStateChangedSlot(const QString & text) {
+    renameMethodFlag = text;
+    qDebug()<< renameMethodFlag;
+}
+
+void Rename::getTextSlot(const QString &text) {
+    qDebug()<<"widget当前的text为："<< text;
+    currentText = text;
+}
+
+void Rename::openDirSlot() {
+    QFileInfo fileInfo(currentText);
+    QString filePath = fileInfo.absolutePath();
+    qDebug()<<"filePath"<<filePath;
+    QString path = filePath.replace("/", "\\");
+    qDebug()<<"path"<<path;
+    QProcess::startDetached("explorer " + path);
+}
+
+void Rename::delActionTriggeredSlot() {
+    QFile file(currentText);
+    qDebug()<<"即将删除："<<currentText;
+    emit delActionFeedbackSignal(file.remove());
+    qDebug()<<"删除成功";
+}
+
---
+++ src/widget.cpp
@@ -0,0 +1,252 @@
+//
+// Created by 12038 on 2022/6/15.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_Widget.h" resolved
+
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+        : QWidget(parent)
+        , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    //取消菜单栏
+    this->setWindowFlags(Qt::FramelessWindowHint);
+
+    //阴影边框效果
+    QGraphicsDropShadowEffect *shadow = new QGraphicsDropShadowEffect();
+    shadow->setBlurRadius(10);
+    shadow->setColor(Qt::black);
+    shadow->setOffset(0);
+
+    ui->shadowWidget->setGraphicsEffect(shadow);
+
+    //父窗口透明
+    this->setAttribute(Qt::WA_TranslucentBackground);
+
+    //最大化最小化关闭功能实现
+    connect(ui->btnMax, SIGNAL(clicked()), this, SLOT(btnMaxClickedSlot()));
+    connect(ui->btnMin, SIGNAL(clicked()), this, SLOT(btnMinClickedSlot()));
+    connect(ui->btnClose, SIGNAL(clicked()), this, SLOT(btnCloseClickedSlot()));
+
+    ui->btnMin->setStyleSheet("border-image: url(:/png/min.png)");
+    ui->btnMax->setStyleSheet("border-image: url(:/png/fullscreen3.png)");
+    ui->btnClose->setStyleSheet("border-image: url(:/png/close.png)");
+
+
+    renameFile = new Rename();
+    myThread = new QThread();
+    renameFile->moveToThread(myThread);
+    myThread->start();
+    connect(renameFile, SIGNAL(destroyed(QObject *)),
+            myThread, SLOT(deleteLater()));
+
+//    ui->labelTitle->setText("      文件重命名");
+    connect(ui->btnSelectFile, SIGNAL(clicked(bool)), this, SLOT(selectFileClickedSlot()));
+    connect(ui->btnSelectDir, SIGNAL(clicked(bool)), this, SLOT(selectDirClickedSlot()));
+    connect(this, SIGNAL(fileSelectedSignal(const QString &, const QString &, const QString &)),
+            renameFile, SLOT(renameFileSlot(const QString &, const QString &, const QString &)));
+
+    connect(this, SIGNAL(getFilesSignal(const QString &, const QString &, const QString &)),
+            renameFile, SLOT(getFilesSlot(const QString &, const QString &, const QString &)));
+
+
+    connect(renameFile, SIGNAL(process(const int &, const int &)),
+            this, SLOT(processSlot(const int &, const int &)));
+    connect(renameFile, SIGNAL(oldFileNameSignal(const QString &)),
+            this, SLOT(oldFileNameSlot(const QString &)));
+    connect(renameFile, SIGNAL(newFileNameSignal(const QString &)),
+            this, SLOT(newFileNameSlot(const QString &)));
+    connect(renameFile, SIGNAL(singleFileSignal(const QString &)),
+            this, SLOT(newFileNameSlot(const QString &)));
+
+    connect(ui->comboBox, SIGNAL(currentTextChanged(QString)),
+            renameFile, SLOT(comboxStateChangedSlot(const QString &)));
+
+    connect(ui->btnConfirm, SIGNAL(clicked()),
+            this, SLOT(btnConfirmClickedSlot()));
+
+    ui->listWidget_2->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(ui->listWidget_2, SIGNAL(customContextMenuRequested(const QPoint &)),
+            this, SLOT(on_listWidget_customContextMenuRequested(const QPoint &)));
+
+    delAction = new QAction();
+    delAction->setText("删除文件");
+    openAction = new QAction();
+    openAction->setText("打开文件目录");
+
+    connect(delAction, SIGNAL(triggered(bool)),
+            renameFile, SLOT(delActionTriggeredSlot()));
+    connect(ui->listWidget_2, SIGNAL(currentTextChanged(const QString &)),
+            renameFile, SLOT(getTextSlot(const QString &)));
+    connect(openAction, SIGNAL(triggered(bool)),
+            renameFile, SLOT(openDirSlot()));
+    connect(renameFile, SIGNAL(delActionFeedbackSignal(bool)),
+            this, SLOT(delActionFeedbackSlot(bool)));
+}
+
+Widget::~Widget()
+{
+    renameFile->deleteLater();
+    myThread->exit();
+    myThread->wait(10 * 1000);
+    openAction->deleteLater();
+    delAction->deleteLater();
+    delete ui;
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+//    QWidget::mousePressEvent(event);
+    QPoint mouseStartPoint = event->globalPos();
+    QPoint windowLeftTopPoint = this->geometry().topLeft();
+    this->mousePosInWindow = mouseStartPoint - windowLeftTopPoint;
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+//    QWidget::mouseMoveEvent(event);
+    if(this->mousePosInWindow == QPoint()) return;
+    QPoint mousePoint = event->globalPos();
+    QPoint windowLeftTopPoint = mousePoint - this->mousePosInWindow;
+    this->move(windowLeftTopPoint);
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *)
+{
+    this->mousePosInWindow = QPoint();
+}
+
+void Widget::closeEvent(QCloseEvent *event)
+{
+    QMessageBox::StandardButton button;
+    button=QMessageBox::question(this,tr("退出程序"),QString(tr("确认退出程序?")),QMessageBox::Yes|QMessageBox::No);
+    if(button==QMessageBox::No)
+    {
+        event->ignore(); // 忽略退出信号，程序继续进行
+    }
+    else if(button==QMessageBox::Yes)
+    {
+        event->accept(); // 接受退出信号，程序退出
+    }
+}
+
+void Widget::btnMaxClickedSlot()
+{
+    ui->btnMax->setStyleSheet("border-image: url(:/png/fullscreen4.png)");
+    if(this->isMaximized()){
+        ui->layoutMain->setMargin(9);
+        ui->btnMax->setStyleSheet("border-image: url(:/png/fullscreen3.png)");
+        this->showNormal();
+    }
+    else{
+        ui->layoutMain->setMargin(0);
+        ui->btnMax->setStyleSheet("border-image: url(:/png/fullscreen4.png)");
+        this->showMaximized();
+    }
+}
+
+void Widget::btnMinClickedSlot()
+{
+    this->showMinimized();
+}
+
+void Widget::btnCloseClickedSlot()
+{
+    this->close();
+}
+
+void Widget::selectFileClickedSlot() {
+    if(!confirmFlag){
+        QMessageBox::information(this, "提示", "请先确认配置信息：模式为"+ui->comboBox->currentText());
+        return;
+    }
+    QString path = QFileDialog::getOpenFileName(
+            this, "选择文件",
+            "",
+            "");
+    if (path == "") return;
+
+    ui->progressBar->setValue(0);
+    ui->listWidget->clear();
+    ui->listWidget_2->clear();
+    ui->listWidget->addItem(path);
+
+    emit fileSelectedSignal(path, ui->leMd5Show->text(), ui->lineEdit->text());
+}
+
+void Widget::selectDirClickedSlot() {
+    if(!confirmFlag){
+        QMessageBox::information(this, "提示", "请先确认配置信息：模式为"+ui->comboBox->currentText());
+        return;
+    }
+    QString dirPathUrl = QFileDialog::getExistingDirectory(this, "选择文件夹", "");
+    ui->lineDIrShow->setText(dirPathUrl);
+    if (dirPathUrl == "") return;
+    ui->progressBar->setValue(0);
+    ui->listWidget->clear();
+    ui->listWidget_2->clear();
+//    ui->leMd5Show->clear();
+    emit getFilesSignal(dirPathUrl, ui->leMd5Show->text(), ui->lineEdit->text());
+}
+
+void Widget::processSlot(const int &now, const int &total) {
+    ui->progressBar->setMaximum(total);
+    ui->progressBar->setValue(now);
+}
+
+void Widget::oldFileNameSlot(const QString &oldName) {
+    ui->listWidget->addItem(oldName);
+}
+
+void Widget::newFileNameSlot(const QString &newName) {
+    ui->listWidget_2->addItem(newName);
+}
+
+void Widget::btnConfirmClickedSlot() {
+    QString s = ui->lineEdit->text();
+    if(s.contains('/') || s.contains('\\') || s.contains('<') || s.contains('>') || s.contains('?') || s.contains('*') || s.contains(':') || s.contains('|') || s.contains('"')){
+        QMessageBox::critical(this, "错误", "文件名中不能包含/\\|<>:?*等字符");
+        return;
+    }
+    confirmFlag = true;
+}
+
+void Widget::on_listWidget_customContextMenuRequested(const QPoint &pos)
+{
+//    ui->listWidget_2->currentTextChanged()
+    QMenu *menu = new QMenu(this);
+//    menu->addAction(ui->actionAdd);
+//    menu->addAction(ui->actionClear);
+//    menu->addAction(ui->actionDelete);
+//    menu->addAction(ui->actionInsert);
+
+    menu->addAction(this->openAction);
+    menu->addAction(this->delAction);
+//    menu->addSeparator();
+
+//    menu->addAction(ui->actionInit);
+//    menu->addSeparator();
+//    menu->addAction(ui->actionSelAll);
+//    menu->addAction(ui->actionSelInv);
+//    menu->addAction(ui->actionSelNone);
+//    menu->addAction(ui->actionSelPopMenu);
+    menu->exec(QCursor::pos());
+    delete  menu;
+}
+
+void Widget::delActionFeedbackSlot(bool flag) {
+    if(flag){
+        qDebug()<<"remove item";
+        QListWidgetItem * item = ui->listWidget_2->currentItem();
+        qDebug()<<item;
+        ui->listWidget_2->removeItemWidget(item);
+        delete item;
+    }
+}
+
+
+
+
